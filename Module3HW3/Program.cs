a6340d994646b0ece3bd5d9bfa5b0d6ffdad83e0
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-12-13 16:41:24 +0200

    Done.

--- Module3HW3/Program.cs
+++ Module3HW3/Program.cs
@@ -6,6 +6,16 @@ namespace Module3HW3
     {
         public static void Main(string[] args)
         {
+            var class1 = new Class1();
+            var class2 = new Class2();
+
+            class1.ShowDelegateHandler = Show;
+            class1.ShowDelegateHandler(class2.Calc(class1.PowDelegateHandler, 4, 4));
+        }
+
+        public static void Show(Class2.ResultDelegate resultDelegate)
+        {
+            Console.WriteLine(resultDelegate(8));
         }
     }
 }
